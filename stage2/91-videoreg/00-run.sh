b81c0f1bc377c8ae5eca282d86d3095fd30fc36a
vrgorg <vrgorg@github.com>
2026-04-27 08:55:48 +0300

    Change .img name format

--- stage2/91-videoreg/00-run.sh
+++ stage2/91-videoreg/00-run.sh
@@ -11,6 +11,15 @@ EOF
 
 git clone --depth 1 https://github.com/videoreg/pi-videoreg.git "${ROOTFS_DIR}/${VIDEOREG_DIR}"
 
+VIDEOREG_VERSION_FILE="${ROOTFS_DIR}/${VIDEOREG_DIR}/VERSION"
+if [ -f "${VIDEOREG_VERSION_FILE}" ]; then
+  VIDEOREG_VERSION=$(cat "${VIDEOREG_VERSION_FILE}" | tr -d '[:space:]')
+  echo "videoreg-${VIDEOREG_VERSION}" > "${WORK_DIR}/img_name"
+  echo "PiVideoreg version: ${VIDEOREG_VERSION}"
+else
+  echo "WARNING: VERSION file not found in pi-videoreg repo"
+fi
+
 on_chroot << EOF
   chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} "${VIDEOREG_DIR}"
 
